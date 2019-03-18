8b33dd2d4e80e3e90b05243c93eed65840423d47
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-18 12:05:08 -0700

    added clients icon about to make table

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 
 export default class Clients extends Component {
 	render() {
-		return (
-			<div>
-				<h1>Clients</h1>
-			</div>
-		);
+		const clients = [
+			{
+				id: '123',
+				firstName: 'Kevin',
+				lastName: 'John',
+				email: 'a@a',
+				phone: '555',
+				balance: '30'
+			}
+		];
+		if (clients) {
+			return (
+				<div>
+					<div className="row">
+						<div className="col-md-6">
+							<h2>
+								{' '}
+								 <i className="fas fa-users" />Clients {' '}
+							</h2>
+						</div>
+						<div className="col-md-6" />
+					</div>
+				</div>
+			);
+		} else {
+			return <h1>Loading... </h1>;
+		}
 	}
 }
