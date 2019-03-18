e5b6c6fa137f194ac554cd61ec52321a87322b48
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-18 12:12:23 -0700

    dashboard client and sidebar components + fields showing now

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+// i for icon use bootstrap class
 export default class Clients extends Component {
 	render() {
 		const clients = [
@@ -19,11 +20,37 @@ export default class Clients extends Component {
 						<div className="col-md-6">
 							<h2>
 								{' '}
-								 <i className="fas fa-users" />Clients {' '}
+								<i className="fas fa-users" />Clients {' '}
 							</h2>
 						</div>
 						<div className="col-md-6" />
 					</div>
+					<table className="table table-striped">
+						<thead className="thead-inverse">
+							<tr>
+								<th>Name</th>
+								<th>Email</th>
+								<th>Balance</th>
+								<th />
+							</tr>
+						</thead>
+						<tbody>
+							{clients.map((client) => (
+								<tr key={client.id}>
+									<td>
+										{client.firstName} {client.lastName}
+									</td>
+									<td>{client.email}</td>
+									<td>${parseFloat(client.balance).toFixed(2)}</td>
+									<td>
+										<Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
+											<i className="fas fa-arrow-circle-right" />
+										</Link>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</div>
 			);
 		} else {
