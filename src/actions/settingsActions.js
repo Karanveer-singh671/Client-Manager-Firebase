81bac841cb5c98efe11208d587e736869662b468
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-19 13:10:43 -0700

    functions in settings actions done

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -17,12 +17,32 @@ export const setDisableBalanceOnAdd = () => {
 };
 
 export const setDisableBalanceOnEdit = () => {
+	// Get settings from localStorage
+	const settings = JSON.parse(localStorage.getItem('settings'));
+
+	// Toggle
+	settings.disableBalanceOnEdit = !settings.disableBalanceOnEdit;
+
+	// Set back to localStorage
+	localStorage.setItem('settings', JSON.stringify(settings));
+
 	return {
-		type: DISABLE_BALANCE_ON_EDIT
+		type: DISABLE_BALANCE_ON_EDIT,
+		payload: settings.disableBalanceOnEdit
 	};
 };
+
 export const setAllowRegistration = () => {
+	// Get settings from localStorage
+	const settings = JSON.parse(localStorage.getItem('settings'));
+
+	// Toggle
+	settings.allowRegistration = !settings.allowRegistration;
+
+	// Set back to localStorage
+	localStorage.setItem('settings', JSON.stringify(settings));
 	return {
-		type: ALLOW_REGISTRATION
+		type: ALLOW_REGISTRATION,
+		payload: settings.allowRegistration
 	};
 };
