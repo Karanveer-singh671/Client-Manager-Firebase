65d409ce8a0c2652150d82403a762f808c2c2cd6
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-19 11:52:18 -0700

    added userIS and is not authenticated for Appjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -10,6 +11,10 @@ import AddClient from './components/clients/AddClient';
 import EditClient from './components/clients/EditClient';
 import ClientDetails from './components/clients/ClientDetails';
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Settings from './components/settings/Settings';
+
+import './App.css';
 
 class App extends Component {
 	render() {
@@ -20,11 +25,12 @@ class App extends Component {
 						<AppNavbar />
 						<div className="container">
 							<Switch>
-								<Route exact path="/" component={Dashboard} />
-								<Route exact path="/client/add" component={AddClient} />
-								<Route exact path="/client/edit/:id" component={EditClient} />
-								<Route exact path="/client/:id" component={ClientDetails} />
-								<Route exact path="/login" component={Login} />
+								<Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+								<Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+								<Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+								<Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
+								<Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
+								/>
 							</Switch>
 						</div>
 					</div>
