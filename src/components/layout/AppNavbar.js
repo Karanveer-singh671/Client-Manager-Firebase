735579be5d86d85c0f3b4a2565b0ff10ae4dab77
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-19 12:50:59 -0700

    added setting mark up in appnavbar component

--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
@@ -19,6 +19,7 @@ class AppNavbar extends Component {
 			return { isAuthenticated: false };
 		}
 	}
+
 	onLogoutClick = (e) => {
 		e.preventDefault();
 
@@ -29,11 +30,13 @@ class AppNavbar extends Component {
 	render() {
 		const { isAuthenticated } = this.state;
 		const { auth } = this.props;
+		const { allowRegistration } = this.props.settings;
+
 		return (
 			<nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
 				<div className="container">
 					<Link to="/" className="navbar-brand">
-						ClientManager
+						ClientPanel
 					</Link>
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain">
 						<span className="navbar-toggler-icon" />
@@ -49,11 +52,38 @@ class AppNavbar extends Component {
 							) : null}
 						</ul>
 						{isAuthenticated ? (
-							<li className="nav-item">
-								<Link to="/" className="nav-link">
-									Dashboard
-								</Link>
-							</li>
+							<ul className="navbar-nav ml-auto">
+								<li className="nav-item">
+									<a href="#!" className="nav-link">
+										{auth.email}
+									</a>
+								</li>
+								<li className="nav-item">
+									<Link to="/settings" className="nav-link">
+										Settings
+									</Link>
+								</li>
+								<li className="nav-item">
+									<a href="#!" className="nav-link" onClick={this.onLogoutClick}>
+										Logout
+									</a>
+								</li>
+							</ul>
+						) : null}
+
+						{allowRegistration && !isAuthenticated ? (
+							<ul className="navbar-nav ml-auto">
+								<li className="nav-item">
+									<Link to="/login" className="nav-link">
+										Login
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link to="/register" className="nav-link">
+										Register
+									</Link>
+								</li>
+							</ul>
 						) : null}
 					</div>
 				</div>
@@ -61,9 +91,11 @@ class AppNavbar extends Component {
 		);
 	}
 }
+
 AppNavbar.propTypes = {
 	firebase: PropTypes.object.isRequired,
-	auth: PropTypes.object.isRequired
+	auth: PropTypes.object.isRequired,
+	settings: PropTypes.object.isRequired
 };
 
 export default compose(
