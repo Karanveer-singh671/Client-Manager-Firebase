f6e4da3650199ceb20c3a20309115a7d709f19d6
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-19 12:16:22 -0700

    added checks for  message and notifyUser

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
-
-import React, { Component } from 'react';
+import { notifyUser } from '../../actions/notifyActions';
+import Alert from '../layout/Alert';
 
 class Login extends Component {
 	state = {
 		email: '',
 		password: ''
 	};
+
 	onSubmit = (e) => {
 		e.preventDefault();
-		const { firebase } = this.props;
+
+		const { firebase, notifyUser } = this.props;
 		const { email, password } = this.state;
 
 		firebase
@@ -27,11 +29,13 @@ class Login extends Component {
 	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
 	render() {
+		const { message, messageType } = this.props.notify;
 		return (
 			<div className="row">
 				<div className="col-md-6 mx-auto">
 					<div className="card">
 						<div className="card-body">
+							{message ? <Alert message={message} messageType={messageType} /> : null}
 							<h1 className="text-center pb-4 pt-3">
 								<span className="text-primary">
 									<i className="fas fa-lock" /> Login
@@ -69,8 +73,19 @@ class Login extends Component {
 		);
 	}
 }
-Login.PropTypes = {
-	firebase: PropTypes.object.isRequired
+
+Login.propTypes = {
+	firebase: PropTypes.object.isRequired,
+	notify: PropTypes.object.isRequired,
+	notifyUser: PropTypes.func.isRequired
 };
 
-export default firebaseConnect()(Login);
+export default compose(
+	firebaseConnect(),
+	connect(
+		(state, props) => ({
+			notify: state.notify
+		}),
+		{ notifyUser }
+	)
+)(Login);
