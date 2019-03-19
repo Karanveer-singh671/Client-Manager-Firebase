d7c7c896d0ac0e923ed69198394457e105204812
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-19 12:59:23 -0700

    setDisableBalanceOnAdd in settingsAction for local storage and to give payload to reducer

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -1,10 +1,21 @@
 import { DISABLE_BALANCE_ON_ADD, DISABLE_BALANCE_ON_EDIT, ALLOW_REGISTRATION } from './types';
 
 export const setDisableBalanceOnAdd = () => {
+	const settings = JSON.parse(localStorage.getItem('settings'));
+
+	// Toggle
+	settings.disableBalanceOnAdd = !settings.disableBalanceOnAdd;
+
+	// Set back to localStorage
+	localStorage.setItem('settings', JSON.stringify(settings));
+
 	return {
-		type: DISABLE_BALANCE_ON_ADD
+		type: DISABLE_BALANCE_ON_ADD,
+		// give payload to reducer
+		payload: settings.disableBalanceOnAdd
 	};
 };
+
 export const setDisableBalanceOnEdit = () => {
 	return {
 		type: DISABLE_BALANCE_ON_EDIT
